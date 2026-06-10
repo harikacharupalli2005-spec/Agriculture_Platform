401b8cc2faaf189f31a6e579a22edce792640d25
Harika Charupalli <harikacharupalli2005@gmail.com>
2026-06-10 11:25:47 +0530

    Add backend config folder

---
+++ backend/config/passport.js
@@ -0,0 +1,47 @@
+const passport = require("passport");
+const GoogleStrategy =
+  require("passport-google-oauth20").Strategy;
+
+const User = require("../models/User");
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    },
+
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const email = profile.emails[0].value;
+
+        let user = await User.findOne({
+          email: email,
+        });
+
+        if (!user) {
+          user = await User.create({
+            name: profile.displayName,
+            email: email,
+            phone: "Not Provided",
+            village: "Not Provided",
+            district: "Not Provided",
+            googleId: profile.id,
+            role: "farmer",
+            preferredLanguage: "English",
+          });
+        } else {
+          user.googleId = profile.id;
+          await user.save();
+        }
+
+        return done(null, user);
+      } catch (error) {
+        return done(error, null);
+      }
+    }
+  )
+);
+
+module.exports = passport;
